8dfaf61f05a13954bead6ffd49891d92b20bf057
Manav Madan Rawal <80583498+Manav02012002@users.noreply.github.com>
2022-07-13 14:39:38 +0530

    Add files via upload

---
+++ TemperatureConversion.py
@@ -0,0 +1,217 @@
+def C_to_F(x):
+    t = (x*9/5)+32
+    return t
+def C_to_K(x):
+    t = x+273.15
+    return t
+def C_to_R(x):
+    t = (x*9/5)+491.67
+    return t
+def C_to_N(x):
+    t = x*0.33000
+    return t
+def C_to_Ro(x):
+    t = (x*0.5250) + 7.50
+    return t
+def C_to_D(x):
+    t = (x*1.5000) - 100.00
+    return t
+def C_to_Rea(x):
+    t = (x*0.80000)
+    return t
+def C_to_GM(x):
+    t = (x*9/5)+32
+    return t
+def F_to_C(x):
+    t = (x-32)*5/9
+    return t
+def F_to_K(x):
+    t = (x-32)*5/9 + 273.15
+    return t
+def F_to_R(x):
+    t = x+459.67
+    return t
+def F_to_N(x):
+    t = (x-32)*0.18333
+    return t
+def F_to_Ro(x):
+    t = (x-32)*0.18333
+    return t
+def F_to_D(x):
+    t = (x-32)*0.83333 -100
+    return t
+def F_to_Rea(x):
+    t = (x-32)*0.444444
+    return t
+def F_to_GM(x):
+    t = (x*9/5)+32
+    return t
+def K_to_C(x):
+    t = (x-273.15)
+    return t
+def K_to_F(x):
+    t = (x-273.15)*(9/5)+32
+    return t
+def K_to_R(x):
+    t = (x*1.8)
+    return t
+def K_to_N(x):
+    t = (x-273.15)*0.33000
+    return t
+def K_to_Ro(x):
+    t = (x-273.15)*0.52500 +7.50
+    return t
+def K_to_D(x):
+    t = (x-273.15)*1.5000 - 100.00
+    return t
+def K_to_Rea(x):
+    t = (x-273.15)*0.80000
+    return t
+def K_to_GM(x):
+    t = (x*9/5)+32
+    return t
+def R_to_C(x):
+    t = (x-491.67)/1.8000
+    return t
+def R_to_F(x):
+    t = (x-491.67) + 32.00
+    return t
+def R_to_K(x):
+    t = (x-491.67)/1.8000 + 273.15
+    return t
+def R_to_N(x):
+    t = (x-491.67)*0.18333
+    return t
+def R_to_Ro(x):
+    t = (x-491.67)*(0.29167) +7.50
+    return t
+def R_to_D(x):
+    t = (x-491.67)*(0.83333) -100.00
+    return t
+def R_to_Rea(x):
+    t = (x-491.67)*0.44444
+    return t
+def R_to_GM(x):
+    t = (x*9/5)+32
+    return t
+def N_to_C(x):
+    t = (x*9/5)+32
+    return t
+def N_to_F(x):
+    t = (x*9/5)+32
+    return t
+def N_to_K(x):
+    t = (x*9/5)+32
+    return t
+def N_to_R(x):
+    t = (x*9/5)+32
+    return t
+def N_to_Ro(x):
+    t = (x*9/5)+32
+    return t
+def N_to_D(x):
+    t = (x*9/5)+32
+    return t
+def N_to_Rea(x):
+    t = (x*9/5)+32
+    return t
+def N_to_GM(x):
+    t = (x*9/5)+32
+    return t
+def Ro_to_C(x):
+    t = (x*9/5)+32
+    return t
+def Ro_to_F(x):
+    t = (x*9/5)+32
+    return t
+def Ro_to_K(x):
+    t = (x*9/5)+32
+    return t
+def Ro_to_R(x):
+    t = (x*9/5)+32
+    return t
+def Ro_to_N(x):
+    t = (x*9/5)+32
+    return t
+def Ro_to_D(x):
+    t = (x*9/5)+32
+    return t
+def Ro_to_Rea(x):
+    t = (x*9/5)+32
+    return t
+def Ro_to_GM(x):
+    t = (x*9/5)+32
+    return t
+def D_to_C(x):
+    t = (x*9/5)+32
+    return t
+def D_to_F(x):
+    t = (x*9/5)+32
+    return t
+def D_to_K(x):
+    t = (x*9/5)+32
+    return t
+def D_to_R(x):
+    t = (x*9/5)+32
+    return t
+def D_to_N(x):
+    t = (x*9/5)+32
+    return t
+def D_to_Ro(x):
+    t = (x*9/5)+32
+    return t
+def D_to_Rea(x):
+    t = (x*9/5)+32
+    return t
+def D_to_GM(x):
+    t = (x*9/5)+32
+    return t
+def Rea_to_C(x):
+    t = (x*9/5)+32
+    return t
+def Rea_to_F(x):
+    t = (x*9/5)+32
+    return t
+def Rea_to_K(x):
+    t = (x*9/5)+32
+    return t
+def Rea_to_R(x):
+    t = (x*9/5)+32
+    return t
+def Rea_to_N(x):
+    t = (x*9/5)+32
+    return t
+def Rea_to_Ro(x):
+    t = (x*9/5)+32
+    return t
+def Rea_to_D(x):
+    t = (x*9/5)+32
+    return t
+def Rea_to_GM(x):
+    t = (x*9/5)+32
+    return t
+def GM_to_C(x):
+    t = (x*9/5)+32
+    return t
+def GM_to_F(x):
+    t = (x*9/5)+32
+    return t
+def GM_to_K(x):
+    t = (x*9/5)+32
+    return t
+def GM_to_R(x):
+    t = (x*9/5)+32
+    return t
+def GM_to_N(x):
+    t = (x*9/5)+32
+    return t
+def GM_to_Ro(x):
+    t = (x*9/5)+32
+    return t
+def GM_to_D(x):
+    t = (x*9/5)+32
+    return t
+def GM_to_Rea(x):
+    t = (x*9/5)+32
+    return t
+print(F_to_Rea(100)) 
