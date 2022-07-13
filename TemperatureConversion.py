f7a6947111c4bebd2912c227e4dbce15fd480f25
Manav Madan Rawal <80583498+Manav02012002@users.noreply.github.com>
2022-07-13 15:35:17 +0530

    Add files via upload

--- TemperatureConversion.py
+++ TemperatureConversion.py
@@ -95,67 +95,67 @@ def R_to_GM(x):
     t = (x*9/5)+32
     return t
 def N_to_C(x):
-    t = (x*9/5)+32
+    t = (x/0.33000)
     return t
 def N_to_F(x):
-    t = (x*9/5)+32
+    t = (x*5.4545)+32
     return t
 def N_to_K(x):
-    t = (x*9/5)+32
+    t = (x/0.33000) +273.15
     return t
 def N_to_R(x):
-    t = (x*9/5)+32
+    t = (x*5.4545)+491.67
     return t
 def N_to_Ro(x):
-    t = (x*9/5)+32
+    t = (x*1.5909)+7.50
     return t
 def N_to_D(x):
-    t = (x*9/5)+32
+    t = (x*4.5455)- 100.00
     return t
 def N_to_Rea(x):
-    t = (x*9/5)+32
+    t = (x*2.4242)
     return t
 def N_to_GM(x):
     t = (x*9/5)+32
     return t
 def Ro_to_C(x):
-    t = (x*9/5)+32
+    t = (x-7.5)/0.52500
     return t
 def Ro_to_F(x):
-    t = (x*9/5)+32
+    t = (x-7.5)*(3.4286)+32
     return t
 def Ro_to_K(x):
-    t = (x*9/5)+32
+    t = (x-7.5)/(0.52500) +273.15
     return t
 def Ro_to_R(x):
-    t = (x*9/5)+32
+    t = (x-7.5)*(3.4286)+491.67
     return t
 def Ro_to_N(x):
-    t = (x*9/5)+32
+    t = (x-7.5)*(0.62857)
     return t
 def Ro_to_D(x):
-    t = (x*9/5)+32
+    t = (x-7.5)*(2.8571) -100.00
     return t
 def Ro_to_Rea(x):
-    t = (x*9/5)+32
+    t = (x-7.5)*(1.5238)
     return t
 def Ro_to_GM(x):
     t = (x*9/5)+32
     return t
 def D_to_C(x):
-    t = (x*9/5)+32
+    t = (x+100)/1.5000
     return t
 def D_to_F(x):
-    t = (x*9/5)+32
+    t = (x+100)*1.2000 +32.00
     return t
 def D_to_K(x):
-    t = (x*9/5)+32
+    t = (x+100)/1.5000 +273.15
     return t
 def D_to_R(x):
-    t = (x*9/5)+32
+    t = (x+100)*1.2000 +491.67
     return t
 def D_to_N(x):
-    t = (x*9/5)+32
+    t = (x+100)*0.22000
     return t
 def D_to_Ro(x):
     t = (x*9/5)+32
